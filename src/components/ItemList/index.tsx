b491ba5e9957d1168a4097ddff01c24709eae8e2
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-12 04:27:47 -0300

    Adicionado propspara receber da page as  functions de acesso ao firebase.

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -6,17 +6,16 @@ import * as C from "./styled";
 
 type Props = {
   item: IDados;
+  deleteTarefa: (id: number) => void;
+  updateTarefa: (id: number, name: string) => void;
 };
 
-export function ListItem({ item }: Props) {
+export function ListItem({ item, deleteTarefa, updateTarefa }: Props) {
   const [isChecked, setIsChecked] = useState(Boolean);
+  const [isEdit, setIsEdit] = useState(0);
   const [buttonDisabled, setButtonDisabled] = useState(Boolean);
   const [valueModal, setValueModal] = useState(false);
-
-  useEffect(() => {
-    setButtonDisabled(Boolean)
-  }, [])
-
+  const [newName, setNewName] = useState('')
 
   function handleModalOpen() {
     setValueModal(true);
@@ -26,17 +25,26 @@ export function ListItem({ item }: Props) {
     setValueModal(false);
   }
 
-  async function deleteTask(): Promise<void> {
-    api.delete(`/Cadastro/${item.id}`);
-  }
-
   async function validaDelete(isChecked: boolean) {
     if (isChecked === true) {
       handleModalOpen();
+      setIsEdit(1);
       if (valueModal) {
-        deleteTask();
-        handleModalClose()
-        setIsChecked(false)   
+        deleteTarefa(item.id);
+        handleModalClose();
+        setIsChecked(false);
+      }
+    }
+  }
+
+  async function validaUpdate(isChecked: boolean) {
+    if (isChecked === true) {
+      handleModalOpen();
+      setIsEdit(0);
+      if(valueModal){
+        updateTarefa(item.id, newName);
+        handleModalClose();
+        setIsChecked(false);
       } 
     }
   }
@@ -46,18 +54,55 @@ export function ListItem({ item }: Props) {
     <C.Container done={isChecked}>
       <input
         type="checkbox"
+        className="inputPrincipal"
         checked={isChecked}
         onChange={(e) => setIsChecked(e.target.checked)}
       />
       <label>{item.name}</label>
 
-      <C.DivFlutuante>
+      <C.DivFlutuanteDelete>
         <button
           disabled={buttonDisabled}
           onClick={() => validaDelete(isChecked)}
+          className="buttonDelete"
+          value={isEdit}
         >
           Deletar
         </button>
+      </C.DivFlutuanteDelete>
+      <C.DivFlutuanteEdit>
+        <button
+          disabled={buttonDisabled}
+          onClick={() => validaUpdate(isChecked)}
+          className="buttonEdit"
+          value={isEdit}
+        >
+          Editar
+        </button>
+      </C.DivFlutuanteEdit>
+      {isEdit === 0 ? (
+        <IsModal
+          isOpen={valueModal}
+          onRequestClose={handleModalClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
+        >
+          <h3>Você deseja Editar está tarefa?</h3>
+          <h4 className="h4new">Digite a nova descrição da tarefa:</h4>
+          <C.InputEdit
+            type="text"
+            checked={isChecked}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <C.Button
+            edit={isEdit}
+            className="buttonStyle"
+            onClick={() => validaUpdate(isChecked)}
+          >
+            Confirmar
+          </C.Button>
+        </IsModal>
+      ) : (
         <IsModal
           isOpen={valueModal}
           onRequestClose={handleModalClose}
@@ -65,9 +110,15 @@ export function ListItem({ item }: Props) {
           className="react-modal-content"
         >
           <h3>Você deseja deletar está tarefa?</h3>
-          <C.Button className="buttonStyle" onClick={() => validaDelete(isChecked)} >Confirmar</C.Button>
+          <C.Button
+          edit={isEdit}
+            className="buttonStyle"
+            onClick={() => validaDelete(isChecked)}
+          >
+            Confirmar
+          </C.Button>
         </IsModal>
-      </C.DivFlutuante>
+      )}
     </C.Container>
   );
 }
