8cf30445c0099b2fb60281a2d550d9d945edaecb
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-16 18:24:15 -0300

    retirado o children e adicionado todas as funcionalidades dentro do modal, será feito um generico

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,32 +1,37 @@
 import { ReactNode } from "react";
 import Modal from "react-modal";
 import fechar from "../../assets/x.svg";
-import { Container } from "./styled";
+import { Button, Container } from "./styled";
 
 interface OpenModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
-  overlayClassName: string;
-  className: string;
-  children: ReactNode;
+  functionActions: () => void;
 }
 
-export function IsModal({ isOpen, onRequestClose, overlayClassName, className, children }: OpenModalProps) {
+export function IsModal({
+  isOpen,
+  onRequestClose,
+  functionActions,
+}: OpenModalProps) {
   return (
     <Container>
-      <Modal 
-      isOpen={isOpen} 
-      overlayClassName={overlayClassName}
-      className={className}
+      <Modal
+        isOpen={isOpen}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
         <button
           type="button"
-          onClick={onRequestClose} 
+          onClick={onRequestClose}
           className="react-modal-close"
         >
           <img src={fechar} alt="Fechar Modal" />
         </button>
-        {children}
+        <h3>Você deseja deletar está tarefa?</h3>
+        <Button className="buttonStyle" onClick={() => functionActions()}>
+          Confirmar
+        </Button>
       </Modal>
     </Container>
   );
