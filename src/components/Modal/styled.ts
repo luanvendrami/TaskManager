ca5d2f7a13ce62291e2e5e0152a7de93a38444b2
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-16 18:22:14 -0300

    adicionado styled ao component

--- src/components/Modal/styled.ts
+++ src/components/Modal/styled.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
   button {
     width: 100%;
     padding: 0 1.5rem;
@@ -19,5 +18,25 @@ export const Container = styled.div`
     &:hover {
       filter: brightness(0.9);
     }
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 0 1.5rem;
+  height: 4rem;
+  background: "#555fed";
+  color: #ccc;
+  border-radius: 0.25rem;
+  border: 0;
+  font-size: 1rem;
+  margin-top: 1.5rem;
+  font-weight: 600;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(0.9);
   }
 `;
